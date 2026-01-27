7a0d56e3af8ca4ad436d9fcd3f689f71838d1431
taha-rizvi <mtaharizvi140@gmail.com>
2026-01-28 02:12:50 +0530

    Added test script for perceptron visualization and XOR implementation and it's visualization.

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,10 +1,11 @@
 import numpy as np
 class perceptron:
-    def __init__(self,eta,epsilon):
+    def __init__(self,eta,max_iters):
         self.eta=eta
         self.b=None
         self.w=None
-        self.epsilon=epsilon
+        # self.epsilon=epsilon
+        self.max_iters=max_iters
     def sign(self,z):
         #wrong ! i needed to do elementwise comparison
         # if z>=0:
@@ -14,18 +15,33 @@ class perceptron:
 
     def train(self,X,y):
         m,n=X.shape
-        self.w=np.random.randint(0,1,size=(m,n),dtype=float)
-        self.b=np.zeros((n,),dtype=float)
-        
-        while(y-y_hat>self.epsilon):
-            z=np.matmul(self.w.T,X)+self.b
-            y_hat=self.sign(z)
-            if(y*y_hat<=0):
-                self.w+=self.eta*y*X
-                self.b+=self.eta*y
+        self.w=np.random.randn(n)*0.01
+        self.b=0.0
+        # z=np.matmul(X,self.w)+self.b
+        # y_hat=self.sign(z)
+        # for i in range(m):
+        #     while(y[i]-y_hat[i]>self.epsilon):
+        #         if(y[i]*y_hat[i]<=0):
+        #             self.w+=self.eta*y[i]*X[i]
+        #             self.b+=self.eta*y[i]
+        for _ in range(self.max_iters):
+            n_misclassified=0
+            for i in range(m):
+            
+                z=np.dot(X[i],self.w)+self.b
+                y_hat=self.sign(z)
+
+                if y[i]*y_hat<=0:
+            
+                    self.w+=self.eta*y[i]*X[i]
+                    self.b+=self.eta*y[i]
+                    n_misclassified+=1
+            if n_misclassified==0:
+                break
+
 
     def predict(self,X_test):
-        out=np.matmul(self.w.T,X_test)+self.b
+        out = np.dot(X_test, self.w) + self.b
         return self.sign(out)
         
 
