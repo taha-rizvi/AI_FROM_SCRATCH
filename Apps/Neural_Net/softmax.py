1f5e0f1ab5dd90142139cb536018cc5b7b64e438
taha-rizvi <mtaharizvi140@gmail.com>
2026-02-12 22:38:08 +0530

    Implement softmax forward and backward methods for gradient computation

--- Apps/Neural_Net/softmax.py
+++ Apps/Neural_Net/softmax.py
@@ -4,4 +4,7 @@ class softmax:
 
         pass
     def __call__(self,x):
-        return np.exp(x-np.max(x))/np.sum(np.exp(x-np.max(x)),axis=1, keepdims=True)
+        self.output= np.exp(x-np.max(x))/np.sum(np.exp(x-np.max(x)),axis=1, keepdims=True)
+        return self.output
+    def backward(self,dout):
+        return self.output*(dout-np.sum(dout*self.output,axis=1,keepdims=True))
