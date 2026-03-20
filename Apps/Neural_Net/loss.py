91e04d5d0c22389e1d37c4228232233102aea869
taha-rizvi <mtaharizvi140@gmail.com>
2026-03-21 02:40:02 +0530

    Add binary and categorical cross-entropy loss classes; implement MSE loss function

--- Apps/Neural_Net/loss.py
+++ Apps/Neural_Net/loss.py
@@ -17,3 +17,42 @@ class CrossEntropy:
         dout=gradient/len(self.out)
         return dout
 
+class binaryCrossEntropy:
+    def __init__(self,out,y):
+        self.out=out
+        self.y=y
+    def item(self):
+        eps=1e-9
+        return -np.mean(self.y*np.log(self.out+eps)+(1-self.y)*np.log(1-self.out+eps)).astype(np.float64)
+    
+    def backward(self):
+        eps=1e-9
+        dout = (
+        (self.out - self.y) /
+        ((self.out + eps) * (1 - self.out + eps))
+    ) / len(self.y)
+
+        return dout
+
+class CategoricalCrossEntropy:
+    def __init__(self,out,y):
+        self.y=y
+        self.out=out
+    def item(self):
+        
+        return -np.mean(np.sum(self.y * np.log(self.out), axis=1))
+    def backward(self):   
+    
+        return -self.y / (self.out * len(self.y))
+    
+class MSE:
+    def __init__(self,out,y):
+        self.y=y
+        self.out=out
+    def item(self):
+
+        return np.mean((self.out - self.y)**2)
+    def backward(self):   
+        n=self.out.size
+        return (2 / n) * (self.out - self.y)
+    
